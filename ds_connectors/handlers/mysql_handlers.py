a41b8a3271ac27fd1599165e1284259a7fdd833d
gigas64 <gigas64@opengrass.net>
2023-04-08 15:32:57 -0500

    update mysql handler and tests

--- ds_connectors/handlers/mysql_handlers.py
+++ ds_connectors/handlers/mysql_handlers.py
@@ -1,120 +1,125 @@
-import traceback
-
-from aistac.handlers.abstract_handlers import AbstractSourceHandler, ConnectorContract, HandlerFactory
+from aistac.handlers.abstract_handlers import AbstractSourceHandler, ConnectorContract
+from aistac.handlers.abstract_handlers import HandlerFactory, AbstractPersistHandler
 import pandas as pd
-import re
-import pymysql.cursors
 
-__author__ = 'Johan Gielstra'
+__author__ = 'Darryl and Sekhar'
 
 
-class MySQLSourceHandler(AbstractSourceHandler):
-    """ A MySQL Source Handler"""
+class MysqlSourceHandler(AbstractSourceHandler):
+    """ This handler class uses both SQLAlchemy and pymysql. Together, SQLAlchemy and pymysql provide a powerful
+    toolset for working with databases and faster connectivity. SQLAlchemy allows developers to interact with MySQL
+    using Python code, while MySQL provides the database functionality needed to store and retrieve data efficiently.
+
+        URI example
+            uri = "mysql://name:password@host:port/database?param2=param1&param2=param2"
+
+        params:
+            query: (optional) a source query added as a parameter or kward
+            table: (optional) the sql table assuming full dataframe persist. By default 'hadron_default' is used
+            if_exists: (optional) How to behave if the table already exists.‘fail’, ‘replace’, ‘append’. Default replace
+
+    """
 
     def __init__(self, connector_contract: ConnectorContract):
-        """ initialise the Handler passing the source_contract dictionary """
+        """ initialise the Handler passing the Connector Contract """
         # required module import
         self.pymysql = HandlerFactory.get_module('pymysql')
+        self.sqlalchemy = HandlerFactory.get_module('sqlalchemy')
+        self.sqlalchemy_schema = HandlerFactory.get_module('sqlalchemy.schema')
         super().__init__(connector_contract)
-        self._host, self._port, self._database, self._user, self._password = self.__get_connector_details()
-        self._file_state = 0
+        # reset to use dialect
+        _kwargs = {**self.connector_contract.kwargs, **self.connector_contract.query}
+        self._sql_query = _kwargs.pop('query', "")
+        self._sql_table = _kwargs.pop('table', "hadron_default")
+        self._if_exists = _kwargs.pop('if_exists', 'replace')
+        # add dialect
+        address = self.connector_contract.address.replace('mysql:', 'mysql+pymysql:')
+        self._engine = self.sqlalchemy.create_engine(address, **_kwargs)
+        self._update_time = pd.Timestamp("1970-01-01")
         self._changed_flag = True
 
     def supported_types(self) -> list:
         """ The source types supported with this module"""
-        return ['jdbc', 'mysql']
+        return ['mysql']
 
     def exists(self) -> bool:
-        return True
+        """If the table exists"""
+        try:
+            meta = self.sqlalchemy_schema.MetaData()
+            meta.reflect(bind=self._engine)
+            if self._sql_table in meta.tables.keys():
+                return True
+            return False
+        except self.pymysql.Error:
+            return False
 
     def has_changed(self) -> bool:
-        """ returns if the file has been modified"""
+        """ if the table has changed. Only works with certain implementations"""
         # TODO: Add in change logic here
-        state = None
-        if state != self._file_state:
+        database = str(self.connector_contract.path[1:])
+        table = str(self._sql_table)
+        query = f"SELECT UPDATE_TIME " \
+                f"FROM information_schema.tables " \
+                f"WHERE TABLE_SCHEMA = '{database}' AND TABLE_NAME = '{table}';"
+        with self._engine.connect() as con:
+            result = con.execute(query)
+        last_update = pd.to_datetime((next(result))[0])
+        if last_update is None or self._update_time < last_update:
             self._changed_flag = True
-            self._file_state = state
+        else:
+            self._changed_flag = False
         return self._changed_flag
 
     def reset_changed(self, changed: bool = False):
-        """ manual reset to say the file has been seen. This is automatically called if the file is loaded"""
+        """ manual reset to say the table has been seen. This is automatically called if the file is loaded"""
         changed = changed if isinstance(changed, bool) else False
         self._changed_flag = changed
 
     def load_canonical(self, **kwargs) -> dict:
-        """ returns the canonical dataset based on the source contract
-            The canonical in this instance is a dictionary that has the headers as the key and then
-            the ordered list of values for that header
-        """
-        conn = None
+        """ returns the canonical dataset based on the Connector Contract """
         if not isinstance(self.connector_contract, ConnectorContract):
             raise ValueError("The Connector Contract is not valid")
-
         try:
-            conn = self.pymysql.connect(host=self._host,
-                                        user=self._user,
-                                        password=self._password,
-                                        database=self._database,
-                                        port=int(self._port), cursorclass=pymysql.cursors.DictCursor)
-            # TODO nested modules(pymysql.cursors) are not being loaded using HandlerFactory.get_module('pymysql'),
-            #  had to import manually in imports
+            connect = self._engine.connect()
+            query = self._sql_query if len(self._sql_query) > 0 else f"SELECT * FROM {self._sql_table}"
+            rtn_df = pd.read_sql(query, con=connect, **kwargs)
+            connect.close()
+            return rtn_df
+        except self.pymysql.Error as error:
+            raise ConnectionError(f"Failed to load the canonical to MySQL because {error}")
 
-            # gets query from uri if exists
-            query=""
-            if "query" in self.connector_contract.query:
-                query = self.connector_contract.query['query']
 
-            # check query is passed explicitly other than in uri
-            if self.connector_contract.kwargs.get('query'):
-                query = self.connector_contract.kwargs.get('query')
+class MysqlPersistHandler(MysqlSourceHandler, AbstractPersistHandler):
+    # a MySQL persist handler
 
-            if not query:
-                raise ValueError("Query is not specified in either URI nor Connector Contract")
+    def persist_canonical(self, canonical: pd.DataFrame, **kwargs) -> bool:
+        """ persists the canonical dataset"""
+        return self.backup_canonical(canonical=canonical, table=self._sql_table, **kwargs)
 
-            with conn.cursor() as cursor:
-                cursor.execute(query)
-                result = cursor.fetchall()
-                df = pd.DataFrame(result)
-                return df
-        except (Exception, self.pymysql.Error) as error:
-            print(error)
-            # traceback.print_exc()
-        finally:
-            if conn is not None:
-                conn.close()
-                # print('Database connection closed.')
+    def backup_canonical(self, canonical: pd.DataFrame, table: str, **kwargs) -> bool:
+        """ creates a backup of the canonical to an alternative table  """
+        if not isinstance(self.connector_contract, ConnectorContract):
+            return False
+        try:
+            _if_exists = self._if_exists
+            _params = kwargs
+            _if_exists = _params.pop('if_exists', self._if_exists)
+            connect = self._engine.connect()
+            canonical.to_sql(con=connect, name=table, if_exists=_if_exists, index=False, **_params)
+            connect.close()
+            return True
+        except self.pymysql.Error as error:
+            raise ConnectionError(f"Failed to save the canonical to MySQL because {error}")
 
-    def __get_connector_details(self):
-        """
-        gets connector details like host, port, username, password, etc from uri
-        sample uri: "jdbc:mysql://root:password@localhost:3306/database?query=select* from test"
-        """
-        connector_type = self.connector_contract.schema
-        path = self.connector_contract.path
-
-        if not self.__use_mysql_uri_regex(path):
-            raise ValueError("Uri path must match following pattern, "
-                             "jdbc:mysql://user:password@host:port/database?query=select* from test")
-
-        rest_of_uri_str = path[path.rindex("@") + 1:]
-        credentials_str = path[0:path.rindex("@"):].split("//")[1]
-
-        # gets credentials from uri
-        username = credentials_str.split(":")[0]
-        password = credentials_str.split(":")[1]
-
-        # gets host details
-        host = rest_of_uri_str.split(":")[0]
-        port = rest_of_uri_str.split(":")[1].split("/")[0]
-        database = rest_of_uri_str.split(":")[1].split("/")[1].split("?")[0]
-
-        if connector_type.lower() not in self.supported_types():
-            raise ValueError("The source type '{}' is not supported. see supported_types()".format(connector_type))
-        return host, port, database, username, password
-
-    def __use_mysql_uri_regex(self, uri):
-        """
-        checks uri matches to mysql patten
-        """
-        pattern = re.compile(r"[A-Za-z]+:.*//[A-Za-z0-9]+:.*@.*:[0-9]+/[A-Za-z0-9]+", re.IGNORECASE)
-        return pattern.match(uri)
+    def remove_canonical(self) -> bool:
+        """removes the table and content"""
+        if not isinstance(self.connector_contract, ConnectorContract):
+            return False
+        _cc = self.connector_contract
+        try:
+            query = f"DROP TABLE IF EXISTS {self._sql_table};"
+            with self._engine.connect() as con:
+                con.execute(query)
+        except self.pymysql.Error:
+            return False
+        return True
--- tests/mysql/mysql_handler_test.py
+++ tests/mysql/mysql_handler_test.py
@@ -1,51 +1,189 @@
 import unittest
-import pandas as pd
 import os
-from pprint import pprint
+from pathlib import Path
+import shutil
+import pandas as pd
+from aistac.handlers.abstract_handlers import ConnectorContract
+from ds_discovery import SyntheticBuilder
+from aistac.properties.property_manager import PropertyManager
 
-from ds_connectors.handlers.mysql_handlers import MySQLSourceHandler
-from aistac.handlers.abstract_handlers import ConnectorContract, HandlerFactory
+# Pandas setup
+pd.set_option('max_colwidth', 320)
+pd.set_option('display.max_rows', 100)
+pd.set_option('display.max_columns', 99)
+pd.set_option('expand_frame_repr', True)
 
 
-class MySQLHandlerTest(unittest.TestCase):
+class MysqlHandlerTest(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
 
     def setUp(self):
-        # This is for temporary
-        pass
+        # clean out any old environments
+        for key in os.environ.keys():
+            if key.startswith('HADRON'):
+                del os.environ[key]
+        # Local Domain Contract
+        os.environ['HADRON_PM_PATH'] = os.path.join('working', 'contracts')
+        os.environ['HADRON_PM_TYPE'] = 'json'
+        # Local Connectivity
+        os.environ['HADRON_DEFAULT_PATH'] = Path('working/data').as_posix()
+        # Specialist Component
+        try:
+            os.makedirs(os.environ['HADRON_PM_PATH'])
+        except OSError:
+            pass
+        try:
+            os.makedirs(os.environ['HADRON_DEFAULT_PATH'])
+        except OSError:
+            pass
+        PropertyManager._remove_all()
 
     def tearDown(self):
-        pass
-
-    def test_mysql_connector_with_query_in_uri(self):
-        uri = 'jdbc:mysql://root:admin123@localhost:3306/test?query=select * from names where id="1"'
-
-        self.connector_contract = ConnectorContract(uri=uri, module_name='ds_connectors.handlers.mysql_handlers',
-                                                    handler='MySQLSourceHandler')
-        handler = MySQLSourceHandler(connector_contract=self.connector_contract)
-        result = handler.load_canonical()
-        self.assertTrue(isinstance(result, pd.DataFrame))
-
-    def test_mysql_connector_with_query_argument(self):
-        uri = 'jdbc:mysql://root:admin123@localhost:3306/test?query=select * from names'
-        query = "select * from names"
-
-        self.connector_contract = ConnectorContract(uri=uri, module_name='ds_connectors.handlers.mysql_handlers',
-                                                    handler='MySQLSourceHandler', query=query)
-        handler = MySQLSourceHandler(connector_contract=self.connector_contract)
-        result = handler.load_canonical()
-        self.assertTrue(isinstance(result, pd.DataFrame))
-
-    def test_mysql_connector_with_query_false(self):
-        uri = 'jdbc:mysql://root:admin123@localhost:3306/test'
-        query = "select * from names"
-
-        self.connector_contract = ConnectorContract(uri=uri, module_name='ds_connectors.handlers.mysql_handlers',
-                                                    handler='MySQLSourceHandler')
-        handler = MySQLSourceHandler(connector_contract=self.connector_contract)
         try:
-            result = handler.load_canonical()
-        except Exception as e:
-            print(e)
+            shutil.rmtree('working')
+        except OSError:
+            pass
+
+    def test_handler_default(self):
+        sb = SyntheticBuilder.from_memory()
+        df = self.data(size=1_000)
+        sb.set_persist_uri("mysql://user:pass@localhost:3306/mydb")
+        sb.remove_canonical(sb.CONNECTOR_PERSIST)
+        self.assertFalse(sb.pm.get_connector_handler(sb.CONNECTOR_PERSIST).exists())
+        sb.save_persist_canonical(df)
+        result = sb.load_persist_canonical()
+        self.assertTrue(sb.pm.get_connector_handler(sb.CONNECTOR_PERSIST).exists())
+        self.assertEqual((1000, 6), result.shape)
+        self.assertEqual(['cat', 'num', 'int', 'bool', 'date', 'object'], result.columns.to_list())
+        sb.remove_canonical(sb.CONNECTOR_PERSIST)
+
+    def test_handler_query(self):
+        sb = SyntheticBuilder.from_memory()
+        df = self.data(size=1_000)
+        uri = "mysql://user:pass@localhost:3306/mydb?query=select cat, num from hadron_default"
+        sb.set_persist_uri(uri=uri)
+        sb.remove_canonical(sb.CONNECTOR_PERSIST)
+        sb.save_persist_canonical(df)
+        result = sb.load_persist_canonical()
+        self.assertEqual((1000, 2), result.shape)
+        self.assertEqual(['cat', 'num'], result.columns.to_list())
+        sb.remove_canonical(sb.CONNECTOR_PERSIST)
+
+    def test_handler_table(self):
+        os.environ['HADRON_MYSQL_TABLE'] = 'hadron_table'
+        sb = SyntheticBuilder.from_memory()
+        df = self.data(size=1_000)
+        uri = "mysql://user:pass@localhost:3306/mydb?table=${HADRON_MYSQL_TABLE}"
+        sb.set_persist_uri(uri=uri)
+        sb.remove_canonical(sb.CONNECTOR_PERSIST)
+        self.assertFalse(sb.pm.get_connector_handler(sb.CONNECTOR_PERSIST).exists())
+        sb.save_persist_canonical(df)
+        result = sb.load_persist_canonical()
+        self.assertTrue(sb.pm.get_connector_handler(sb.CONNECTOR_PERSIST).exists())
+        self.assertEqual((1000, 6), result.shape)
+        self.assertEqual(['cat', 'num', 'int', 'bool', 'date', 'object'], result.columns.to_list())
+        sb.remove_canonical(sb.CONNECTOR_PERSIST)
+
+    def test_handler_if_exists_global(self):
+        sb = SyntheticBuilder.from_memory()
+        df = self.data(size=1_000)
+        sb.set_persist_uri("mysql://user:pass@localhost:3306/mydb?if_exists=fail")
+        sb.remove_canonical(sb.CONNECTOR_PERSIST)
+        sb.save_persist_canonical(df)
+        with self.assertRaises(ValueError) as context:
+            sb.save_persist_canonical(df)
+        self.assertTrue("Table 'hadron_default' already exists." in str(context.exception))
+        sb.remove_canonical(sb.CONNECTOR_PERSIST)
+
+    def test_handler_if_exists_direct(self):
+        sb = SyntheticBuilder.from_memory()
+        df = self.data(size=1_000)
+        sb.set_persist_uri("mysql://user:pass@localhost:3306/mydb")
+        sb.remove_canonical(sb.CONNECTOR_PERSIST)
+        sb.save_persist_canonical(df, if_exists='fail')
+        with self.assertRaises(ValueError) as context:
+            sb.save_persist_canonical(df, if_exists='fail')
+        self.assertTrue("Table 'hadron_default' already exists." in str(context.exception))
+        sb.remove_canonical(sb.CONNECTOR_PERSIST)
+
+
+    def test_connector_contract(self):
+        os.environ['mysql_user'] = 'user'
+        os.environ['mysql_pass'] = 'pass'
+        os.environ['HADRON_ADDITION'] = 'myAddition'
+        uri = "mysql://${mysql_user}:${mysql_pass}@localhost:3306/mydb?query=select * from compare_target"
+        cc = ConnectorContract(uri=uri, module_name='', handler='', addition='${HADRON_ADDITION}')
+        print(f"raw_uri = {cc.raw_uri}")
+        print(f"raw_kwargs = {cc.raw_kwargs}")
+        print(f"address = {cc.address}")
+        print(f"schema = {cc.schema}")
+        print(f"hostname = {cc.hostname}")
+        print(f"port = {cc.port}")
+        print(f"username = {cc.username}")
+        print(f"password = {cc.password}")
+        print(f"path = {cc.path[1:]}")
+        print(f"query")
+        extra =  cc.query.pop('extra', None)
+        print(f" extra = {extra}")
+        sql_query = cc.query.pop('query', None)
+        print(f" sql_query = {sql_query}")
+        print(f"kwargs")
+        addition = cc.kwargs.get('addition', None)
+        print(f" addition = {addition}")
+
+    def test_raise(self):
+        with self.assertRaises(KeyError) as context:
+            env = os.environ['NoEnvValueTest']
+        self.assertTrue("'NoEnvValueTest'" in str(context.exception))
+
+    def data(self, size: int=10_000, complete: bool=False):
+        sb = SyntheticBuilder.from_memory()
+        df = sb.tools.frame_starter(canonical=size)
+        # types
+        df['cat'] = sb.tools.get_category(['SUSPENDED', 'ACTIVE', 'PENDING', 'INACTIVE'], relative_freq=[1, 99, 10, 40], size=size)
+        df['num'] = sb.tools.get_number(0.5, 1.5, size=size)
+        df['int'] = sb.tools.get_number(-1000, 1000, relative_freq=[1, 2, 3, 5, 7, 11, 7, 2, 1], size=size)
+        df['bool'] = sb.tools.get_category([1, 0], relative_freq=[9, 1], size=size)
+        df['date'] = sb.tools.get_datetime(start='2022-12-01', until='2023-03-31', date_format='%Y-%m-%d', ordered=True, size=size)
+        df['object'] = sb.tools.get_sample('us_professions', size=size)
+        if complete:
+            # distributions
+            df['normal'] = sb.tools.get_dist_normal(mean=0, std=1, size=size)  # normal
+            df['bernoulli'] = sb.tools.get_dist_bernoulli(probability=0.2, size=size)  # bool
+            df['gumbel'] = sb.tools.get_distribution(distribution='gumbel', loc=0, scale=0.1, size=size)  # skew
+            df['poisson'] = sb.tools.get_distribution(distribution='poisson', lam=3, size=size)  # category
+            df['poly'] = sb.tools.correlate_polynomial(df, header='num', coefficient=[6, 0, 1])  # curve
+
+            # impute
+            df['cat_null'] = sb.tools.get_category(list('MFU'), relative_freq=[9, 7, 1], quantity=0.9, size=size)
+            df['num_null'] = sb.tools.get_number(0., 1., quantity=0.98, size=size)
+            df['bool_null'] = sb.tools.get_category(['1', '0'], relative_freq=[1, 20], quantity=0.95, size=size)
+            df['date_null'] = sb.tools.get_datetime(start='2022-12-01', until='2023-03-31', date_format='%Y-%m-%d', quantity=0.99, size=size)
+            df['object_null'] = sb.tools.get_string_pattern('(ddd)sddd-ddd', quantity=0.85, size=size)
+
+            # #compare
+            df['unique'] = sb.tools.get_number(from_value=size, to_value=size * 10, at_most=1, size=size)
+            df['date_tz'] = sb.tools.get_datetime(pd.Timestamp('2021-09-01', tz='CET'), pd.Timestamp('2022-01-01', tz='CET'), date_format='%Y-%m-%d', size=size)
+            df['correlate'] = sb.tools.correlate_values(df, header='poly', jitter=0.05)
+            df['outliers'] = sb.tools.correlate_values(df, header='correlate', jitter=1, choice=4)
+            df['dup_num'] = sb.tools.correlate_values(df, header='num')
+            df['dup_date'] = sb.tools.correlate_dates(df, header='date')
+
+            # # others
+            df['single_num'] = sb.tools.get_number(1, 2, size=size)
+            df['single_cat'] = sb.tools.get_category(['CURRENT'], size=size)
+            df['nulls'] = sb.tools.get_number(20.0, quantity=0, size=size)
+            df['nulls_num'] = sb.tools.get_number(20.0, quantity=0.03, size=size)
+            df['nulls_cat'] = sb.tools.get_category(list('XYZ'), quantity=0.01, size=size)
+
+        return df
 
 
 if __name__ == '__main__':
